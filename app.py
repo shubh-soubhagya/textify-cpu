b56a12c28cc3c22d3671e7fd3ad6ab0e0deb1424
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2025-02-11 10:24:12 +0530

    app.py

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
-from ocr import extract_text
-from spelling_corrections import correct_spelling
-from spacings import add_space_after_punctuation
+from python_scripts.ocr import extract_text, ocr
+from python_scripts.spelling_corrections import correct_spelling
+from python_scripts.spacings import add_space_after_punctuation
 from spellchecker import SpellChecker
-from transformers import AutoModel, AutoTokenizer
-from groqllm import clean_text
-import torch
-
+from python_scripts.groqllm import clean_text
 import re
 
-torch_device = torch.device("cpu")
 
 image_file = input("Image path: ")
-# res = model.chat(tokenizer, image_file, ocr_type='ocr')
 text = extract_text(image_file)
 corrected_text = correct_spelling(text)
 final_text = add_space_after_punctuation(corrected_text)
 corrected_text = final_text
-clean_text(corrected_text)
-# print(chat_completion.choices[0].message.content)  
+textify_text= clean_text(corrected_text)
+print(textify_text)  
 
 
